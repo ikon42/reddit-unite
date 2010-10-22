ce2babc758a8dd9e1b4b3a0895db8b47e3bd59f4
indieinvader <killpack99@gmail.com>
2010-10-22 09:11:16 -0500

    We are the Connection Machine now

--- template.py
+++ template.py
@@ -4,7 +4,6 @@ from os import path
 
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
-from jinja2 import TemplateNotFound
 
 template_dir = path.join(path.dirname(__file__), 'templates')
 
